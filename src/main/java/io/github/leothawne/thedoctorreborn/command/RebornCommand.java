50504a6d8b5200064688bfbd0ab8bf0dc06a9219
Build Tools <31146142+leothawne@users.noreply.github.com>
2019-02-23 18:40:42 -0300

    License notes added

--- src/main/java/io/github/leothawne/thedoctorreborn/command/RebornCommand.java
+++ src/main/java/io/github/leothawne/thedoctorreborn/command/RebornCommand.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2019 Murilo Amaral Nappi (murilonappi@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.leothawne.thedoctorreborn.command;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -15,7 +31,6 @@ import io.github.leothawne.thedoctorreborn.ConsoleLoader;
 import io.github.leothawne.thedoctorreborn.TheDoctorRebornLoader;
 import io.github.leothawne.thedoctorreborn.Version;
 import io.github.leothawne.thedoctorreborn.event.RegenerationEvent;
-/*@SuppressWarnings("deprecation")*/
 public class RebornCommand implements CommandExecutor {
 	private TheDoctorRebornLoader plugin;
 	private Connection connection;
